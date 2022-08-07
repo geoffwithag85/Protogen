bf7ea97ca6bf9354c841ca561b11c89b0ec095ec
geoffwithag85 <brown.geoff.l@gmail.com>
2022-08-07 12:16:34 -0400

    ...

---
+++ strats/BBRSI.py
@@ -0,0 +1,3 @@
+import TradingStrat
+
+BBRSI = TradingStrat()
--- strats/TradingStrat.py
+++ strats/TradingStrat.py
@@ -1,4 +1,9 @@
 class TradingStrat:
     def __init__(self, candles):
         self.candles = candles
-        
+        self.buy = False
+        self.sell = False
+
+    def buysellsignals(self):
+
+        return [self.buy, self.sell]
